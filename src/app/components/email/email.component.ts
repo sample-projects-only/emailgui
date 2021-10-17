db0dbc9d74074fdc3887d2acf23fba6e0b31260b
njain51 <official.nitinjain@gmail.com>
2021-10-17 14:50:46 -0400

    adding binding for form fields

--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailComponent implements OnInit {
 
+  data = {
+
+    to : "",
+    subject:"",
+    message:""
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  doSubmitForm()
+  {
+  console.log("try to submit form")
   }
 
 }
